3823829c4c61ed6a78494ba13279e4b3377597b0
dmitry karpenko <dmitry.karpenko@gmail.com>
2016-11-24 22:52:51 +0200

    Initial

---
+++ FileManager.Client/app/directory/directoryCtrl.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    "use strict";
+    angular
+        .module("directoryManagement")
+        .controller("DirectoryCtrl",
+                    ["directoryResource",
+                     DirectoryCtrl]);
+
+    function DirectoryCtrl(directoryResource) {
+        var vm = this;
+
+        directoryResource.query(function (data) {
+            vm.directory = data;
+        })
+    }
+}());
---
+++ FileManager.Client/common/directoryResource.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("common.services")
+        .factory("directoryResource",
+                 ["$resource",
+                  "appSettings",
+                  directoryResource])
+
+    function directoryResource($resource, appSettings) {
+        return $resource(appSettings.serverPath + "/api/directory/:id",
+                         { },
+                         { 'query': { isArray: false } });
+    }
+}());
+
